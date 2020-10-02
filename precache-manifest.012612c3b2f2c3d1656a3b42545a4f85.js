5a800d9ba858acfb6dd7309ac03d81fa5f9653e6
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-10-02 12:08:29 +0200

    Updates

---
+++ precache-manifest.012612c3b2f2c3d1656a3b42545a4f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff19420cd2403c1a2051d6371d11f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e791a6ac989d2bb4a41",
+    "url": "/static/css/main.81e66bab.chunk.css"
+  },
+  {
+    "revision": "7063ee4fe0334dfa2544",
+    "url": "/static/js/2.5f83abe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f83abe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e791a6ac989d2bb4a41",
+    "url": "/static/js/main.15c34be6.chunk.js"
+  },
+  {
+    "revision": "422494d7b3b5aa38b525",
+    "url": "/static/js/runtime-main.5f7e4113.js"
+  },
+  {
+    "revision": "e4f2fd232ec2b4173f65347902b3a968",
+    "url": "/static/media/Background.e4f2fd23.jpg"
+  }
+]);
